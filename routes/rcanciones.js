a3acd2dd48fe8aac5a70fe02f234449fe0f51736
Marcos Fernandez <UO270803@uniovi.es>
2021-03-25 17:57:26 +0100

    SDI-2021-206-7.5-Vistas y motores de plantillas.”

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,25 +1,40 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get('/suma', function(req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
 
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
-
-        if (req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-
-        if (typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: ' + req.query.autor;
+        let canciones = [ {
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        },{
+            "nombre" : "Uptown funk",
+            "precio" : "1.1"
+        }];
+        let respuesta = swig.renderFile('views/btienda.html',{
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
+        });
 
         res.send(respuesta);
     });
+
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
 
     app.get('/canciones/:id', function(req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
+
     app.get('/canciones/:genero/:id', function(req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
