769eeb3cd02171a5a7da6a25fe73d4cfde9047df
Marcos Fernandez <UO270803@uniovi.es>
2021-03-25 17:13:02 +0100

    SDI-2021-206-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -25,4 +25,10 @@ module.exports = function(app) {
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
     });
+
+    app.post("/cancion", function (req, res) {
+        res.send("Cancion agregada: "+req.body.nombre + "<br>" +
+            " genero: " + req.body.genero + "<br>" +
+            " precio: " + req.body.precio);
+    })
 };
