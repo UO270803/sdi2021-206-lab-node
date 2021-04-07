716332e29a0a451a06eb69096f9235cd8f9285d8
Marcos Fernandez <UO270803@uniovi.es>
2021-04-07 19:30:37 +0200

    SDI-2021-206-8.3-Subida a ficheros y gestión de colecciones.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let express = require('express');
 let app = express();
 
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
 let mongo = require('mongodb')
 let swig = require('swig');
 let bodyParser = require('body-parser');
@@ -15,7 +17,7 @@ app.use(express.static('public'));
 
 //Variables
 app.set('port', 8081);
-app.set('db', 'mongodb://admin:admin@tiendamusica-shard-00-00.6rdpk.mongodb.net:27017,tiendamusica-shard-00-01.6rdpk.mongodb.net:27017,tiendamusica-shard-00-02.6rdpk.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12biel-shard-0&authSource=admin&retryWrites=true&w=majority')
+app.set('db', 'mongodb://admin:admin@tiendamusica-shard-00-00.6rdpk.mongodb.net:27017,tiendamusica-shard-00-01.6rdpk.mongodb.net:27017,tiendamusica-shard-00-02.6rdpk.mongodb.net:27017/tiendamusica?ssl=true&replicaSet=atlas-12biel-shard-0&authSource=admin&retryWrites=true&w=majority')
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
