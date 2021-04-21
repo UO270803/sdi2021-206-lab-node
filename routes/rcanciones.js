1607f748947994dfdc3a5b2e35b8adf1ba641b76
Marcos Fernandez <UO270803@uniovi.es>
2021-04-22 00:48:20 +0200

    SDI-2021-206-10.5-SW Consumir SW desde NodeJS

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -29,13 +29,28 @@ module.exports = function (app, swig, gestorBD) {
                         res.send(respuesta);
                     else{
                         usuarioPuedeComprarCancion(req.session.usuario,cancionId,function (comprar) {
-                            respuesta = swig.renderFile('views/bcancion.html',
-                                {
-                                    cancion : canciones[0],
-                                    comentarios : comentarios,
-                                    comprar : comprar
-                                });
-                            res.send(respuesta);
+                            let configuracion = {
+                                url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                                method: "get",
+                                headers: {
+                                    "token": "ejemplo",
+                                }
+                            }
+                            let rest = app.get("rest");
+                            rest(configuracion, function (error, response, body) {
+                                console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                                let objetoRespuesta = JSON.parse(body);
+                                let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                                // nuevo campo "usd"
+                                canciones[0].usd = cambioUSD * canciones[0].precio;
+                                respuesta = swig.renderFile('views/bcancion.html',
+                                    {
+                                        cancion : canciones[0],
+                                        comentarios : comentarios,
+                                        comprar : comprar
+                                    });
+                                res.send(respuesta);
+                            })
                         });
                     }
                 });
